e5ac56f8bc023d9c0136e7b347dbebdfe9b72d1e
Jesuscbart <jcb@jesuscuesta.es>
2023-11-29 01:05:59 +0100

    new

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import express from "express";
-
+/*
 try{
 
+    
     const MONGO_URL = Deno.env.get("MONGO_URL");
   
     if (!MONGO_URL) {
@@ -24,17 +25,19 @@ try{
 
     app.listen(3000, () => console.info("Listening on port 3000. API ready to use"));   //Escucho en el puerto 3000
   
+    
 
+    
   
   }
   catch(e){
     console.error(e);
-
+    Deno.exit(1);
   }   
   
-  
+  */
 
-
+console.log("Hola");
 
 
 
